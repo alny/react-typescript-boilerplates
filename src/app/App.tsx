54daefba74cadcdd11272c19e485276999b263cb
alny <nygaard14@gmail.com>
2019-01-26 15:49:33 +0100

    Basic setup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom";
 import { Hello } from "./components/Hello";
 declare let module: any;
 ReactDOM.render(
-  <Hello compiler="Typescript" framework="React..." bundler="Webpack" />,
+  <Hello compiler={"Typescript"} framework="React..." bundler="Webpack" />,
   document.getElementById("root")
 );
 if (module.hot) {
